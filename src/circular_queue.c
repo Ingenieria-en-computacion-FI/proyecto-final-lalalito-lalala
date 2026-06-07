2916254b3de2cdf219e63b7b631759df914d3c92
emcamguz2709 <emiliocamguz@gmail.com>
2026-06-07 03:46:10 +0000

    circular_queue.c

--- src/circular_queue.c
+++ src/circular_queue.c
@@ -35,9 +35,58 @@ CircularQueue* cq_create(int capacity){
     return q;
 }
 
-void cq_enqueue(CircularQueue* q, int value);
-int cq_dequeue(CircularQueue* q);
-int cq_front(CircularQueue* q);
-int cq_is_empty(CircularQueue* q);
-int cq_is_full(CircularQueue* q);
-void cq_destroy(CircularQueue* q);
+void cq_enqueue(CircularQueue* q, int value) {
+    if (q == NULL || cq_is_full(q)) {
+        return;
+    }
+
+    q->rear = (q->rear + 1) % q->capacity;
+    q->data[q->rear] = value;
+    q->size++;
+}
+
+int cq_dequeue(CircularQueue* q) {
+    if (q == NULL || cq_is_empty(q)) {
+        return -1;
+    }
+
+    int value = q->data[q->front];
+
+    q->front = (q->front + 1) % q->capacity;
+    q->size--;
+
+    return value;
+}
+
+int cq_front(CircularQueue* q) {
+    if (q == NULL || cq_is_empty(q)) {
+        return -1;
+    }
+
+    return q->data[q->front];
+}
+
+int cq_is_empty(CircularQueue* q) {
+    if (q == NULL || q->size == 0) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int cq_is_full(CircularQueue* q) {
+    if (q == NULL) {
+        return 0;
+    }
+
+    return q->size == q->capacity;
+}
+
+void cq_destroy(CircularQueue* q) {
+    if (q == NULL) {
+        return;
+    }
+
+    free(q->data);
+    free(q);
+}
