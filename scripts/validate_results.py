79c1172c4f3766e0711af1d0def801959c49add3
Hec-Mejorada <hectormejorada95@gmail.com>
2026-06-10 22:59:47 -0600

    Update validate_results.py

--- scripts/validate_results.py
+++ scripts/validate_results.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+# Carga el archivo con los resultados
 df = pd.read_csv("../data/benchmarks/benchmark.csv")
 
 print("=== VALIDACIÓN ===")
 
+# Muestra el tiempo promedio por algoritmo
 print(df.groupby("algorithm")["time"].mean())
 
 print("\n✔ OK si no hay valores negativos o NaN")
