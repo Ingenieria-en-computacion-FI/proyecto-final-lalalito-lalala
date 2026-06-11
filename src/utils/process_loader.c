c03309064857631f471f7e0dcfaefb0e3f1810ff
Hec-Mejorada <hectormejorada95@gmail.com>
2026-06-11 09:45:06 -0600

    Update process_loader.c

--- src/utils/process_loader.c
+++ src/utils/process_loader.c
@@ -2,33 +2,32 @@
 #include <stdlib.h>
 #include "process.h"
 
+// Carga procesos desde archivo CSV y devuelve el arreglo
 Process* load_processes(const char* filename, int* count) {
+    FILE* f = fopen(filename, "r");
+    if (!f) {
+        perror("fopen");
+        printf("Intentando abrir: %s\n", filename);
+        return NULL;
+    }
 
-  FILE* f = fopen(filename, "r");
-
-if (!f) {
-    perror("fopen");
-    printf("Intentando abrir: %s\n", filename);
-    return NULL;
-}
-
-    int capacity = 100;
+    int capacity = 100;               // Capacidad inicial
     Process* p = malloc(sizeof(Process) * capacity);
-
     int i = 0;
 
+    // Lee datos: pid, tiempo, prioridad, memoria
     while (fscanf(f, "%d,%d,%d,%d",
                   &p[i].pid,
                   &p[i].burst_time,
                   &p[i].priority,
                   &p[i].memory_required) == 4) {
 
-        // el OS lo inicializa, no Python
+        // Inicializa valores de ejecución
         p[i].remaining_time = p[i].burst_time;
         p[i].state = READY;
 
         i++;
-
+        // Duplica capacidad si es necesario
         if (i >= capacity) {
             capacity *= 2;
             p = realloc(p, sizeof(Process) * capacity);
@@ -36,7 +35,6 @@ if (!f) {
     }
 
     fclose(f);
-
-    *count = i;
+    *count = i;  // Guarda cantidad de procesos leídos
     return p;
 }
