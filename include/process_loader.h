ea034904e59a0c8088ebb63e96f33e46496cc5e4
Hec-Mejorada <hectormejorada95@gmail.com>
2026-06-10 22:51:58 -0600

    Update process_loader.h

--- include/process_loader.h
+++ include/process_loader.h
@@ -1,8 +1,10 @@
+// process_loader.h
 #ifndef PROCESS_LOADER_H
 #define PROCESS_LOADER_H
 
 #include "process.h"
 
+// Carga procesos desde un archivo y devuelve el arreglo; count recibe la cantidad
 Process* load_processes(const char* filename, int* count);
 
 #endif
