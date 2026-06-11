100baaee66094a0d025433c960334ca793f6be12
Hec-Mejorada <hectormejorada95@gmail.com>
2026-06-10 22:49:16 -0600

    Update linked_list.h

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,21 +1,15 @@
-//linked_list.h
+// linked_list.h
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-typedef struct LinkedList LinkedList;
+typedef struct LinkedList LinkedList; // Estructura de lista enlazada simple
 
-LinkedList* linked_list_create();
-
-void linked_list_insert_end(LinkedList* list, int value);
-
-void linked_list_insert_front(LinkedList* list, int value);
-
-int linked_list_remove_front(LinkedList* list);
-
-int linked_list_search(LinkedList* list, int value);
-
-int linked_list_is_empty(LinkedList* list);
-
-void linked_list_destroy(LinkedList* list);
+LinkedList* linked_list_create();          // Crea lista vacía
+void linked_list_insert_end(LinkedList* list, int value);   // Inserta al final
+void linked_list_insert_front(LinkedList* list, int value); // Inserta al inicio
+int linked_list_remove_front(LinkedList* list);             // Elimina y retorna el primero
+int linked_list_search(LinkedList* list, int value);        // Busca un valor
+int linked_list_is_empty(LinkedList* list);                  // Verifica si está vacía
+void linked_list_destroy(LinkedList* list);                  // Libera la memoria
 
 #endif
