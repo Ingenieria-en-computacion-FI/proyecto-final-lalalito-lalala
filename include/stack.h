1af7dfae11075c0e1951f8e645a1558e9c19b451
Hec-Mejorada <hectormejorada95@gmail.com>
2026-06-10 22:55:57 -0600

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,14 +1,14 @@
-//stack.h
+// stack.h
 #ifndef STACK_H
 #define STACK_H
 
-typedef struct Stack Stack;
+typedef struct Stack Stack; // Estructura de pila
 
-Stack* stack_create();
-void stack_push(Stack* stack, int value);
-int stack_pop(Stack* stack);
-int stack_peek(Stack* stack);
-int stack_is_empty(Stack* stack);
-void stack_destroy(Stack* stack);
+Stack* stack_create();       // Crea pila vacía
+void stack_push(Stack* stack, int value); // Agrega elemento arriba
+int stack_pop(Stack* stack); // Elimina y retorna el último elemento
+int stack_peek(Stack* stack); // Consulta el elemento superior
+int stack_is_empty(Stack* stack); // Verifica si está vacía
+void stack_destroy(Stack* stack); // Libera memoria
 
 #endif
